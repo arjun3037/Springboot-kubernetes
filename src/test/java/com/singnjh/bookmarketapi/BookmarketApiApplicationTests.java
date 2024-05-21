d17386c3017f54aa17df981bbd42ec3e9d90771e
arjsingh4 <arjun3037@gmail.com>
2024-05-21 14:33:02 +0530

    initial commit

---
+++ src/test/java/com/singnjh/bookmarketapi/BookmarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singnjh.bookmarketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
